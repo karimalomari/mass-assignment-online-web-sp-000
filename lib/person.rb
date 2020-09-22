55f671939efb5c0c3f0d039654c7e9f5e2804219
Karim Alomari <karim.alomari@gmail.com>
2020-09-22 12:04:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,3 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, 
 end
